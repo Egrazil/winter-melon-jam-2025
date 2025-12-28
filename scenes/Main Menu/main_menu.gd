9bfef27bedeb21fdc21525f57c8e735a03a55aaf
The World Again <108901749+The-World-Again@users.noreply.github.com>
2025-12-27 23:14:00 -0800

    got a really basic main menu going

---
+++ scenes/Main Menu/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+func _on_level_1_pressed() -> void:
+	# When we actually have a level, put its path into the string
+	#get_tree().change_scene_to_file("res://scenes/")
+	pass
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
