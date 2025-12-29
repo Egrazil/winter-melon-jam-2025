d18dbb0ee44c94b8643b89cc3bed1eaa4f2c5cee
The World Again <108901749+The-World-Again@users.noreply.github.com>
2025-12-29 01:29:35 -0800

    final touch ups

--- scenes/levels/light-test/wall_ledge.gd
+++ scenes/levels/light-test/wall_ledge.gd
@@ -5,11 +5,18 @@ extends MeshInstance3D
 @onready var light_detector: RayCast3D = $"light-detector"
 @onready var submesh: GeometryInstance3D = $mesh
 
+var offMaterial: Material
+var onMaterial: Material
 
 func _ready() -> void:
 	light_detector.light = light
 	#light_detector.enter_light.connect(_on_lightdetector_enter_light)
 	#light_detector.exit_light.connect(_on_lightdetector_exit_light)
+	offMaterial = Material.new()
+	#offMaterial.albedo_color = Color("FFFFFF")
+	onMaterial = Material.new()
+	#onMaterial.albedo_color = Color("390261")
+	#$mesh
 	
 	
 func set_light(newLight: Node3D):
@@ -22,10 +29,13 @@ func set_light(newLight: Node3D):
 func _on_lightdetector_enter_light() -> void:
 	submesh.scale.x = 0.1
 	$StaticBody3D/CollisionShape3D.scale.x = 0.1
-
+	#$mesh.get_active_material(0).albedo_color = Color("390261")
+	#$mesh.set_surface_override_material(0,onMaterial)
 	print("enter light")
 
 func _on_lightdetector_exit_light() -> void:
 	submesh.scale.x = 1
 	$StaticBody3D/CollisionShape3D.scale.x = 1
+	#$mesh.get_active_material(0).albedo_color = Color("FFFFFF")
+	#$mesh.set_surface_override_material(0,onMaterial)
 	print("exit light")
