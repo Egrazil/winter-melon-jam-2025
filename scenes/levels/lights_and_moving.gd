1135285e27968db714c4d439d53f3053658dbaf7
The World Again <108901749+The-World-Again@users.noreply.github.com>
2025-12-29 00:05:44 -0800

    Got the basic gameplay working

---
+++ scenes/levels/lights_and_moving.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if Input.is_physical_key_pressed(KEY_R):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		get_tree().change_scene_to_file("res://scenes/Main Menu/main menu.tscn")
